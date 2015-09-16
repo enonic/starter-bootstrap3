a738348b4c39b82dc629100b55789e7a960b1fd6
striky <tlo@enonic.com>
2015-09-16 13:14:20 +0200

    Added layouts

---
+++ src/main/resources/site/layouts/layout-4-col/layout-4-col.js
@@ -0,0 +1,62 @@
+var UTIL = require('/lib/enonic/util/util');
+var portal = require('/lib/xp/portal');
+var thymeleaf = require('/lib/xp/thymeleaf');
+
+// Handle GET request
+exports.get = handleGet;
+
+function handleGet(req) {
+    var me = this;
+
+    function renderView() {
+        var view = resolve('layout-4-col.html');
+        var model = createModel();
+
+        return {
+            body: thymeleaf.render(view, model)
+        };
+    }
+
+    function createModel() {
+        me.component = portal.getComponent();
+
+        var model = {};
+        model.regions = getRegionsWithColumnInfo();
+
+        return model;
+    }
+
+    function getColumnConfig() {
+        var columnConfig = '25-25-25-25';
+        if (me.component.config.columnConfig) {
+            columnConfig = me.component.config.columnConfig;
+        }
+
+        return columnConfig;
+    }
+
+    function getColumnClasses() {
+        var columnConfig = getColumnConfig();
+        var columnPercentages = columnConfig.split('-');
+        var columnClasses = [];
+        for (var i = 0, len = columnPercentages.length; i < len; i++) {
+            var columnClass = Math.round((columnPercentages[i] / 100) * 12);
+            columnClasses.push('col-md-' + columnClass);
+        }
+
+        return columnClasses;
+    }
+
+    function getRegionsWithColumnInfo() {
+        var regions = UTIL.region.get();
+        var columnClasses = getColumnClasses();
+
+        for (var i = 0, len = regions.length; i < len; i++) {
+            regions[i].columnClass = columnClasses[i];
+        }
+
+        return regions;
+    }
+
+    return renderView();
+}
