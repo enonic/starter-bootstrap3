b51cb0d110b96371b9604925dcaa31419a3f662b
Tor Løkken <tor.lokken@gmail.com>
2016-02-23 14:45:41 +0100

    Updated DemoInitializer

--- src/main/java/com/enonic/xp/demo/initializer/DemoInitializer.java
+++ src/main/java/com/enonic/xp/demo/initializer/DemoInitializer.java
@@ -10,20 +10,21 @@ import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.enonic.xp.app.ApplicationKey;
 import com.enonic.xp.content.ApplyContentPermissionsParams;
 import com.enonic.xp.content.Content;
+import com.enonic.xp.content.ContentConstants;
 import com.enonic.xp.content.ContentPath;
 import com.enonic.xp.content.ContentService;
 import com.enonic.xp.content.UpdateContentParams;
 import com.enonic.xp.context.ContextAccessor;
 import com.enonic.xp.context.ContextBuilder;
+import com.enonic.xp.context.Context;
 import com.enonic.xp.export.ExportService;
 import com.enonic.xp.export.ImportNodesParams;
 import com.enonic.xp.export.NodeImportResult;
+import com.enonic.xp.index.IndexService;
 import com.enonic.xp.node.NodePath;
 import com.enonic.xp.security.PrincipalKey;
-import com.enonic.xp.security.PrincipalKeys;
 import com.enonic.xp.security.RoleKeys;
 import com.enonic.xp.security.SecurityService;
 import com.enonic.xp.security.User;
@@ -53,6 +54,8 @@ public class DemoInitializer
 
     private SecurityService securityService;
 
+    private IndexService indexService;
+
     private final Logger LOG = LoggerFactory.getLogger( DemoInitializer.class );
 
     private static final PrincipalKey SUPER_USER_KEY = PrincipalKey.ofUser( UserStoreKey.system(), "su" );
@@ -61,10 +64,23 @@ public class DemoInitializer
     public void initialize()
         throws Exception
     {
-        runAsSuperUser( () -> {
-            doInitialize();
-            return null;
-        } );
+
+        if ( this.indexService.isMaster() )
+        {
+            runAs( createInitContext(), () -> {
+                doInitialize();
+                return null;
+            } );
+        }
+    }
+
+    private Context createInitContext()
+    {
+        return ContextBuilder.from( ContextAccessor.current() ).
+            authInfo( AuthenticationInfo.create().principals( RoleKeys.CONTENT_MANAGER_ADMIN ).user( User.ANONYMOUS ).build() ).
+            branch( ContentConstants.BRANCH_DRAFT ).
+            repositoryId( ContentConstants.CONTENT_REPO.getId() ).
+            build();
     }
 
     private void doInitialize()
@@ -77,17 +93,14 @@ public class DemoInitializer
         }
 
         final Bundle bundle = FrameworkUtil.getBundle( this.getClass() );
-        final ApplicationKey appKey = ApplicationKey.from( bundle );
+
         final VirtualFile source = VirtualFiles.from( bundle, "/import" );
-        final VirtualFile xsltTransformer = VirtualFiles.from( bundle, "/import/replace_app.xsl" );
 
         final NodeImportResult nodeImportResult = this.exportService.importNodes( ImportNodesParams.create().
             source( source ).
             targetNodePath( NodePath.create( "/content" ).build() ).
             includeNodeIds( true ).
             dryRun( false ).
-            xslt( xsltTransformer ).
-            xsltParam( "applicationId", appKey.toString() ).
             build() );
 
         logImport( nodeImportResult );
@@ -162,25 +175,14 @@ public class DemoInitializer
         this.securityService = securityService;
     }
 
-    private <T> T runAsSuperUser( final Callable<T> runnable )
+    @Reference
+    public void setIndexService( final IndexService indexService )
     {
-        final AuthenticationInfo authInfo = getSuperUserAuthInfo();
-        return ContextBuilder.from( ContextAccessor.current() ).authInfo( authInfo ).build().callWith( runnable );
+        this.indexService = indexService;
     }
 
-    private AuthenticationInfo getSuperUserAuthInfo()
+    private <T> T runAs( final Context context, final Callable<T> runnable )
     {
-        return runAs( RoleKeys.ADMIN, () -> {
-            final User superUser = this.securityService.getUser( SUPER_USER_KEY ).
-                orElseThrow( () -> new RuntimeException( "User " + SUPER_USER_KEY + " not found." ) );
-            final PrincipalKeys principals = this.securityService.getMemberships( SUPER_USER_KEY );
-            return AuthenticationInfo.create().principals( principals ).user( superUser ).build();
-        } );
-    }
-
-    private <T> T runAs( final PrincipalKey role, final Callable<T> runnable )
-    {
-        final AuthenticationInfo authInfo = AuthenticationInfo.create().principals( role ).user( User.ANONYMOUS ).build();
-        return ContextBuilder.from( ContextAccessor.current() ).authInfo( authInfo ).build().callWith( runnable );
+        return context.callWith( runnable );
     }
 }
